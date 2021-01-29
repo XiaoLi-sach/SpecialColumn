739d968820c2689a7ab0292a9837f9b6c0bd359a
XiaoLi_1456 <XiaoLi_1456@outlook.com>
2021-01-29 23:11:22 +0800

    修改上个提交出现的bug

--- helper.ts
+++ helper.ts
@@ -1,4 +1,4 @@
-import { ColumnProps, ImageProps, UserProps } from ''
+import { ImageProps } from '@/store'
 
 export function generateFitUrl (data: ImageProps, width: number, height: number, format = ['m_pad']) {
   if (data && data.url) {
@@ -6,17 +6,6 @@ export function generateFitUrl (data: ImageProps, width: number, height: number,
       return current + ',' + prev
     }, '')
     data.fitUrl = data.url + `?x-oss-process=image/resize,${formatStr}h_${height},w_${width}`
-  }
-}
-
-export function addColumnAvatar (data: ColumnProps | UserProps, width: number, height: number) {
-  if (data.avatar) {
-    generateFitUrl(data.avatar, width, height)
-  } else {
-    const parseCol = data as ColumnProps
-    data.avatar = {
-      fitUrl: require(parseCol.title ? '@/assets/column.jpg' : '@/assets/avatar.jpg')
-    }
   }
 }
 
