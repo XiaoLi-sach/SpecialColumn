87d9ba7e712e987f3f2d51727769bef43ef44428
XiaoLi_1456 <XiaoLi_1456@outlook.com>
2021-02-06 15:53:30 +0800

    优化代码格式

--- src/components/ColumnList.vue
+++ src/components/ColumnList.vue
@@ -3,7 +3,7 @@
     <div v-for="column in columnList" :key="column._id" class="col-4 mb-4">
       <div class="card h-100 shadow-sm">
         <div class="card-body text-center">
-          <img :src="column.avatar && column.avatar.url" :alt="column.title" class="rounded-circle border border-light my-3">
+          <img :src="column.avatar && column.avatar.fitUrl" :alt="column.title" class="rounded-circle border border-light my-3" >
           <h5 class="card-title">{{column.title}}</h5>
           <p class="card-text text-left">{{column.description}}</p>
           <router-link :to="`/column/${column._id}`" class="btn btn-outline-primary">进入专栏</router-link>
@@ -12,10 +12,10 @@
     </div>
   </div>
 </template>
-
 <script lang="ts">
 import { defineComponent, PropType, computed } from 'vue'
-import { ColumnProps } from '../store'
+import { ColumnProps } from '@/store'
+import { addColumnAvatar } from '@/helper'
 
 export default defineComponent({
   name: 'ColumnList',
@@ -28,13 +28,7 @@ export default defineComponent({
   setup (props) {
     const columnList = computed(() => {
       return props.list.map(column => {
-        if (!column.avatar) {
-          column.avatar = {
-            url: require('@/assets/column.jpg')
-          }
-        } else {
-          column.avatar.url = column.avatar.url + '?x-oss-process=image/resize,m_pad,h_50,w_50'
-        }
+        addColumnAvatar(column, 50, 50)
         return column
       })
     })
@@ -44,7 +38,6 @@ export default defineComponent({
   }
 })
 </script>
-
 <style scoped>
 .card-body img {
   width: 50px;
--- src/views/CreatePost.vue
+++ src/views/CreatePost.vue
@@ -1,24 +1,28 @@
 <template>
   <div class="create-post-page">
-    <h4>新建文章</h4>
+    <h4>{{isEditMode ? '编辑文章' : '新建文章'}}</h4>
     <uploader
       action="/upload"
       :beforeUpload="uploadCheck"
       @file-uploaded="handleFileUploaded"
+      :uploaded="uploadedData"
       class="d-flex align-items-center justify-content-center bg-light text-secondary w-100 my-4"
     >
-    <h2>点击上传图片</h2>
-    <template #loading>
-      <div class="d-flex">
-        <div class="spinner-border text-secondary" role="status">
-          <span class="sr-only"></span>
+      <h2>点击上传图片</h2>
+      <template #loading>
+        <div class="d-flex">
+          <div class="spinner-border text-secondary" role="status">
+            <span class="visually-hidden">Loading...</span>
+          </div>
+          <h2>正在上传</h2>
         </div>
-        <h2>正在上传</h2>
-      </div>
-    </template>
-    <template #uploaded="dataProps">
-      <img :src="dataProps.uploadedData.data.url">
-    </template>
+      </template>
+      <template #uploaded="dataProps">
+        <div class="uploaded-area">
+          <img :src="dataProps.uploadedData.data.url" alt="">
+          <h3>点击重新上传</h3>
+        </div>
+      </template>
     </uploader>
     <validate-form @form-submit="onFormSubmit">
       <div class="mb-3">
@@ -40,33 +44,36 @@
         />
       </div>
       <template #submit>
-        <button class="btn btn-primary btn-large">发表文章</button>
+        <button class="btn btn-primary btn-large">{{isEditMode ? '更新文章' : '发表文章'}}
+        </button>
       </template>
     </validate-form>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue'
+import { defineComponent, ref, onMounted } from 'vue'
 import { useStore } from 'vuex'
-import { useRouter } from 'vue-router'
+import { useRouter, useRoute } from 'vue-router'
 import { GlobalDataProps, PostProps, ResponseType, ImageProps } from '@/store'
 import ValidateInput, { RulesProp } from '../components/ValidateInput.vue'
 import ValidateForm from '../components/ValidateForm.vue'
-import Uploader from '@/components/Uploader.vue'
+import Uploader from '../components/Uploader.vue'
+import createMessage from '../components/createMessage'
 import { beforeUploadCheck } from '@/helper'
-import createMessage from '@/components/createMessage'
-
 export default defineComponent({
-  name: 'Login',
+  name: 'CreatePost',
   components: {
     ValidateInput,
     ValidateForm,
     Uploader
   },
   setup () {
+    const uploadedData = ref()
     const titleVal = ref('')
     const router = useRouter()
+    const route = useRoute()
+    const isEditMode = !!route.query.id
     const store = useStore<GlobalDataProps>()
     let imageId = ''
     const titleRules: RulesProp = [
@@ -76,6 +83,18 @@ export default defineComponent({
     const contentRules: RulesProp = [
       { type: 'required', message: '文章详情不能为空' }
     ]
+    onMounted(() => {
+      if (isEditMode) {
+        store.dispatch('fetchPost', route.query.id).then((rawData: ResponseType<PostProps>) => {
+          const currentPost = rawData.data
+          if (currentPost.image) {
+            uploadedData.value = { data: currentPost.image }
+          }
+          titleVal.value = currentPost.title
+          contentVal.value = currentPost.content || ''
+        })
+      }
+    })
     const handleFileUploaded = (rawData: ResponseType<ImageProps>) => {
       if (rawData.data._id) {
         imageId = rawData.data._id
@@ -94,10 +113,15 @@ export default defineComponent({
           if (imageId) {
             newPost.image = imageId
           }
-          store.dispatch('createPost', newPost).then(() => {
+          const actionName = isEditMode ? 'updatePost' : 'createPost'
+          const sendData = isEditMode ? {
+            id: route.query.id,
+            payload: newPost
+          } : newPost
+          store.dispatch(actionName, sendData).then(() => {
             createMessage('发表成功，2秒后跳转到文章', 'success', 2000)
             setTimeout(() => {
-              router.push({ name: 'coulmn', params: { id: column } })
+              router.push({ name: 'column', params: { id: column } })
             }, 2000)
           })
         }
@@ -107,10 +131,10 @@ export default defineComponent({
       const result = beforeUploadCheck(file, { format: ['image/jpeg', 'image/png'], size: 1 })
       const { passed, error } = result
       if (error === 'format') {
-        createMessage('上传图片的格式只能是 jpg/png ', 'error')
+        createMessage('上传图片只能是 JPG/PNG 格式!', 'error')
       }
       if (error === 'size') {
-        createMessage('上传图片的大小不能大于1mb ', 'error')
+        createMessage('上传图片大小不能超过 1Mb', 'error')
       }
       return passed
     }
@@ -121,21 +145,32 @@ export default defineComponent({
       contentRules,
       onFormSubmit,
       uploadCheck,
-      handleFileUploaded
+      handleFileUploaded,
+      uploadedData,
+      isEditMode
     }
   }
 })
 </script>
 
 <style>
-.create-post-page .file-upload-container {
-  height: 200px;
-  cursor: pointer;
-}
-
 .create-post-page .file-upload-container img {
   width: 100%;
   height: 100%;
   object-fit: cover;
+}
+.uploaded-area {
+  position: relative;
+}
+.uploaded-area:hover h3 {
+  display: block;
+}
+.uploaded-area h3 {
+  display: none;
+  position: absolute;
+  color: #999;
+  text-align: center;
+  width: 100%;
+  top: 50%;
 }
 </style>
